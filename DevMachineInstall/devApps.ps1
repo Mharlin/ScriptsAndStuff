aafbb4088dafc664c7d88e8efda802675209ded4
Magnus Härlin <magnus.harlin@gmail.com>
2014-06-04 08:32:13 +0200

    uppdaterat scriptet för att passa min miljö

--- DevMachineInstall/devApps.ps1
+++ DevMachineInstall/devApps.ps1
@@ -1,39 +1,20 @@
-# Dev apps to install
-
-# Visual studio stuff
-cinstm VisualStudio2013Ultimate
-cinstm VisualStudio2012Ultimate
 
 # Visual studio add-ins and stuff
-cinstm xUnit.vs2012
-cinstm WebEssentials2012
-cinstm resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
-cinstm typescript
-cinstm typescript.vs
-cinstm SpecFlow
-
-# databases and tools
-cinstm MsSqlServer2012Express
-cinstm mongodb
-cinstm MongoVUE
+cinst WebEssentials2013
+cinst resharper 
+cinst SpecFlow
 
 # browsers
-cinstm Firefox
-cinstm GoogleChrome
+cinst Firefox
+cinst GoogleChrome
 
 # Other tools
-cinstm git
-cinstm gitextensions
-cinstm githubforwindows
-cinstm notepadplusplus
-cinstm winmerge
-cinstm Console2
-cinstm 7zip
-cinstm nodejs.install
-cinstm fiddler
-cinstm curl
-cinstm linqpad4
-cinstm scriptcs
-cinstm sublimetext2
-cinstm SublimeText2.PackageControl
-cinstm ccleaner
+cinst git.install
+cinst gitextensions
+cinst github
+cinst sourcetree
+cinst notepadplusplus
+cinst conemu
+cinst 7zip
+cinst fiddler
+cinst curl
